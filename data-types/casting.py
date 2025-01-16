c038bddb489d5dcc856010c2b2dd0c02f41187aa
obkin <kapellwork@gmail.com>
2025-01-16 15:02:36 +0200

    casting

---
+++ data-types/casting.py
@@ -0,0 +1,14 @@
+# Casting to integer:
+print(int(1.9)) # 1
+print('12') #12
+
+
+# Casting to float:
+print(float(2)) # 2.0
+print(float("28")) # 28.0
+
+
+# Casting to string:
+print(str(1)) # '1'
+print(str(2.0)) # '2.0'
+print(str('s1')) # 's1'
